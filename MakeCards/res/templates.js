53a2301b9171c633abd67d543a57b957b3bdfc9c
Pale Tangent <paletangent@163.com>
2025-05-04 04:12:59 +0800

    v3.0.5

---
+++ MakeCards/res/templates.js
@@ -0,0 +1,8 @@
+var TEMPLATES = [
+	{name: "经典模板（卤粉）", path: "classic/lufen"},
+	{name: "经典模板（自定配色）", path: "classic/custom-colored"},
+	{name: "经典模板（卡牌）", path: "classic/cards"},
+	{name: "国战模板", path: "guozhan"},
+	{name: "三国杀DIY2020模板", path: "diy2020"},
+	{name: "桌游志·☆SP模板", path: "star-sp"},
+];
